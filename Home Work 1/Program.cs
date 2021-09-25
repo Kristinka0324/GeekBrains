a864dce5fc96a6218f26907db802596e2442d654
Kristina <kristinka.antipova94@gmail.com>
2021-09-26 00:51:56 +0500

    my home work

---
+++ Home Work 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Home_Work_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"[{DateTime.Now}] Hello, {name}!");
+
+        }
+    }
+}
