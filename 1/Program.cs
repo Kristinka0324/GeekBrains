c1a542d9d33f8f5ff73f8db53f268486b4a1b3ff
Kristina <kristinka.antipova94@gmail.com>
2021-09-26 10:34:05 +0500

    2

---
+++ 1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What's your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine($"[{DateTime.Now}] Hello, {name}!");
+
+        }
+    }
+}
