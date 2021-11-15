6510d4b496a26ed4809173d83b25c6a170131aca
Kristina <kristinka.antipova94@gmail.com>
2021-11-15 23:22:05 +0500

    DZ_2

---
+++ DZ_2_(1)/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace DZ_2__1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double mintemp, maxtemp;  // a=минимальная темп, b=максимальная температура
+
+            Console.WriteLine("Введите число 1");
+
+            mintemp = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число 2");
+
+            maxtemp = double.Parse(Console.ReadLine());
+
+            double result = (mintemp + maxtemp) / 2;
+
+            Console.WriteLine("среднесуточная температура = " + result);
+
+        }
+    }
+}
